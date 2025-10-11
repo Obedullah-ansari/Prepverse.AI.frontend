23b1e73cf19ca6f918e011188494b75c6a3bb287
Obedullah Ansari <154742900+Obedullah-ansari@users.noreply.github.com>
2025-10-11 15:35:49 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-const clientId =
-  "414745886535-jdjfvlqsnbulip8ir5j5tld45lp4nu28.apps.googleusercontent.com";
+
 import { AuthProvider } from "./context/Authcontext.tsx";
 
 createRoot(document.getElementById("root")!).render(
